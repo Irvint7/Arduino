c78e0f36a9a2aa70b47907c70bfeea08d8761e4f
Ludwin Hernández <alduvi11@gmail.com>
2016-06-06 21:58:41 -0600

    Create servo.ino

---
+++ servo.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+Servo myservo; 
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+myservo.attach(9); 
+myservo.write(90);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int valor=myservo.read();
+myservo.write(45);
+delay(200);
+myservo.write(90);
+delay(2000);
+myservo.write(250);
+delay(1500);
+Serial.println(valor);
+
+}
