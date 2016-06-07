422c281ec077a8023dba62dcd8d1a2c3876ef5b8
Ludwin Hernández <alduvi11@gmail.com>
2016-06-06 22:31:21 -0600

    Update servo.ino

--- servo.ino
+++ servo.ino
@@ -2,21 +2,22 @@
 Servo myservo; 
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-myservo.attach(9); 
-myservo.write(90);
+	// put your setup code here, to run once:
+	Serial.begin(9600);
+	myservo.attach(9); 
+	myservo.write(90);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-int valor=myservo.read();
-myservo.write(45);
-delay(200);
-myservo.write(90);
-delay(2000);
-myservo.write(250);
-delay(1500);
-Serial.println(valor);
+	int valor=myservo.read();
+	myservo.write(45);
+	delay(200);
+	myservo.write(90);
+	delay(2000);
+	myservo.write(250);
+	delay(1500);
+	Serial.println(valor);
 
 }
+
